9e63246a48ca901e39f58202db0afc7b1973d5e9
Robert Dill <bdbdbd@Roberts-MacBook-Pro.local>
2022-03-09 14:29:40 -0500

    initial push of notebook

---
+++ common_OSX.sh
@@ -0,0 +1,37 @@
+ #!/bin/bash
+ 
+ YELLOW='\033[1;33m'
+ RED='\033[1;31m'
+ GREEN='\033[1;32m'
+ RESET='\033[0m'
+ GREY='\033[2m'
+
+# indent text on echo
+function indent() {
+  c='s/^/       /'
+  case $(uname) in
+    Darwin) sed -l "$c";;
+    *)      sed -u "$c";;
+  esac
+}
+
+OS=$(uname)
+export OS=$OS
+
+# displays where we are, uses the indent function (above) to indent each line
+function showStep ()
+    {
+        echo -e "${GREY}=====================================================" | indent
+        echo -e "${RESET}-----> $*" | indent
+        echo -e "${GREY}=====================================================${RESET}" | indent
+    }
+
+# Grab the current directory
+function getCurrent() 
+    {
+        showStep "getting current directory"
+        DIR="$( pwd )"
+        echo "DIR in getCurrent is: ${DIR}"
+        THIS_SCRIPT=`basename "$0"`
+        showStep "Running '${THIS_SCRIPT}'"
+    }
